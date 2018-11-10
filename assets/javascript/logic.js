63c7ee5d051b0baa48190f54a369b8a279ebe8f8
Harrison Palmer <harrisonpalmer@harrisons-mbp.attlocal.net>
2018-11-10 17:59:53 -0600

    update

---
+++ assets/javascript/logic.js
@@ -0,0 +1,15 @@
+var config = {
+    apiKey: "AIzaSyCZQrzLerJqPwgYl9h4sM9H6nD6DCaV73A",
+    authDomain: "train-scheduler-9731a.firebaseapp.com",
+    databaseURL: "https://train-scheduler-9731a.firebaseio.com",
+    projectId: "train-scheduler-9731a",
+    storageBucket: "train-scheduler-9731a.appspot.com",
+    messagingSenderId: "869494458342"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  $( "#submitBtn" ).click(function() {
+
+  });
